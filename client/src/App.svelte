36794203cd7243bf86ab56a0c4e18b70c4af5eb3
Link-69 <jean.faure@outlook.com>
2021-03-13 19:49:00 +0100

    client interface

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,15 +1,47 @@
 <script>
-	import Test from "./Test.svelte";
-	export let name;
+	import FilePicker from './FilePicker.svelte';
+	import axios from 'axios';
+
+	let analyzeResult;
+	let analyzeError;
+
+	const uploadFile = async (e) => {
+		analyzeResult = new Array();
+		analyzeError = '';
+		try {
+			const formData = new FormData();
+
+			formData.append('slpFile', e.detail);
+			const response = await axios.post('http://localhost:3000/upload-slp', formData, {
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
+			});
+			analyzeResult = response.data;
+		} catch (error) {
+			analyzeError = error.response.data;
+		}
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>SLP Pause Checker</h1>
+	<FilePicker on:analyze={uploadFile} />
+	{#if analyzeResult}
+		{#each analyzeResult as result}
+			<div>{result}</div>
+		{/each}
+	{/if}
+	{#if analyzeError}
+		<div class="analyze-error">{analyzeError}</div>
+	{/if}
 </main>
-<Test/>
 
 <style>
+	:global(body) {
+		background-color: #222222;
+		color: #ffffff;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -18,10 +50,14 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #44a963;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.analyze-error {
+		color: #ff0000;
 	}
 
 	@media (min-width: 640px) {
---
+++ client/src/FilePicker.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    let files;
+    const dispatch = createEventDispatcher();
+
+    const analyzeFile = () => {
+        dispatch('analyze', files[0]);
+    }
+</script>
+
+<div>
+	<div>
+		<input type="file" bind:files />
+        {#if files && files[0]}
+		    <button on:click={analyzeFile}>Analyze</button>
+        {/if}
+	</div>
+</div>
